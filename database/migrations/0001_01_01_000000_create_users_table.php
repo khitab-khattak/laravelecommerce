a9e1944a38aac3385390a1368ca6011f572a8759
Khitab-khattakgit commit -m first <khitabk29@gmail.com>
2025-06-29 15:51:24 +0500

    Changing Admin and User Dashboard Layout

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('mobile');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('utype')->default('USR')->comment('USR for user and customer and ADM for admin');
